d29b135e5f53d9d714a1627d692ca4e7a36141b5
Oleksii Sribnyi <me@codemanki.com>
2016-01-16 20:33:54 +0100

    Seeting lib

---
+++ app/libs/settings.js
@@ -0,0 +1,33 @@
+const DEFAULT_SETTINGS = {
+  skipInto: false
+};
+
+let settings = getSettings();
+
+export default const AppSettings = {
+  getItem: (item) => {
+    return settings[item];
+  },
+
+  getAll: () => {
+    return settings;
+  }
+
+  save: (item, value) => {
+    settings[item] = value;
+    localStorage.setItem('settings', JSON.stringify(settings));
+  }
+}
+
+function getSettings() {
+  let result;
+  try {
+    result = JSON.parse(localStorage.settings);
+  } catch () {
+    // No settings, save defaults
+    localStorage.setItem('settings', JSON.stringify(DEFAULT_SETTINGS));
+    result = DEFAULT_SETTINGS;
+  }
+
+  return result;
+}
